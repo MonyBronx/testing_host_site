0ad0a84ed1d34461bbbb5ba236cfb0627b857a3b
Sathyamony <sathyamony.sen@bronxtechnology.com>
2024-06-07 13:57:23 +0700

    web app update #1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "810cb3dc5e21426de5b76076ec1bbbe6",
+const RESOURCES = {"flutter_bootstrap.js": "d2481a6bd2644f5b4214f8548c2f844e",
 "version.json": "466bb8c3128d8934a1e8c6f16aabece7",
-"index.html": "5351c3dbc60c74077d54eb497d4f3646",
-"/": "5351c3dbc60c74077d54eb497d4f3646",
-"main.dart.js": "b70eadb4459eed4397b363601b46f825",
+"index.html": "8eb88d245177de82781a83b6632bffc3",
+"/": "8eb88d245177de82781a83b6632bffc3",
+"main.dart.js": "b00dc6a230d84f05c610c5c324ae4f6e",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
