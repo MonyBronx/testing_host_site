f4bf6cf610997ee99c052aa01510045758c89a8b
Sathyamony <sathyamony.sen@bronxtechnology.com>
2024-06-07 08:47:01 +0700

    Remove sharedpreference

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b8b26ef1a052ea8075c7d74c040ec18b",
+const RESOURCES = {"flutter_bootstrap.js": "fef32ad3522cae47ac2b0170d7938002",
 "version.json": "466bb8c3128d8934a1e8c6f16aabece7",
-"index.html": "7607917c27150aa59679265016554e93",
-"/": "7607917c27150aa59679265016554e93",
-"main.dart.js": "9bd2e22cef0ec55d2a5097b8b5485f24",
+"index.html": "6aa0f75f79e42e407839992a78d8771d",
+"/": "6aa0f75f79e42e407839992a78d8771d",
+"main.dart.js": "b700adf42e882310e72c0fe254c20200",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
