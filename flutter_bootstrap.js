10b3cbafdeef655231c868a9d67e5bc2bf181717
Sathyamony <sathyamony.sen@bronxtechnology.com>
2024-06-25 15:35:07 +0700

    update web app #33

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3501678663"
+    serviceWorkerVersion: "4078402731"
   }
 });
